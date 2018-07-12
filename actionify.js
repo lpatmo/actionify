8912a8c04c5e636f5241eee563183f2af5081307
ISPOL <insignificantpointoflight@gmail.com>
2018-07-12 16:43:06 -0400

    account pages

--- actionify.js
+++ actionify.js
@@ -6,3 +6,33 @@
 
 browser.browserAction.onClicked.addListener(openPage);
 console.log("hi");*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('RecommendBtn').addEventListener('click', recommendActions);
+    document.getElementById('SubmitBtn').addEventListener('click', submitActions);      
+    document.getElementById('CancelBtn').addEventListener('click', cancelActions);
+    let currentTab = browser.tabs.query({active: true, currentWindow:true});     	
+    let urltxt = document.getElementById('newsURL');
+    
+    currentTab.then(setURL)
+    function setURL(tab){
+    	urltxt.value=tab[0].url;
+
+    }
+
+});
+
+function recommendActions(){
+	document.getElementById("TakeAction").style.display="none";
+	document.getElementById("SubmitForm").style.display="block";
+	document.querySelector("body").style.width="230px";
+}
+function cancelActions(){
+	document.getElementById("TakeAction").style.display="block";
+	document.getElementById("SubmitForm").style.display="none";
+	document.querySelector("body").style.width="100%";
+}
+function submitActions(){
+	//form stuff
+	cancelActions();
+}
--- app/actionifythenews/actionify/urls.py
+++ app/actionifythenews/actionify/urls.py
@@ -1,4 +1,4 @@
-"""actionifythenews URL Configuration
+"""actionify URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -17,9 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.conf.urls import url
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('actionify.urls')),
+	path('', views.index, name='index'),
 ]
